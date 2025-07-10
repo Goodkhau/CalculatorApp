63819ed53dcb0f5563d78fb1fd15309b494e39d6
Goodkhau <gos.khai12@gmail.com>
2025-07-10 18:41:52 -0400

    Display and clearDisplay functions updated.

--- CalculatorApp/CalculatorFunctions.js
+++ CalculatorApp/CalculatorFunctions.js
@@ -1,3 +1,42 @@
 function display(input) {
-    console.log(input);
+    const element = document.getElementById("calculatorDisplay");
+    if (element.value.length == 0) {
+        if (typeof input === "number") {
+            element.value = input;
+        }
+        return;
+    }
+
+    let equationList = element.value.split(" ");
+    if (equationList.length == 1) {
+        if (typeof input === "number") {
+            element.value += input;
+        }
+        if (typeof input === "string") {
+            element.value += " " + input;
+        }
+        return;
+    }
+
+    if (equationList.length == 2) {
+        if (typeof input === "number") {
+            element.value += " " + input;
+        }
+        if (typeof input === "string") {
+            element.value.charAt(element.value.length) = input;
+        }
+        return;
+    }
+
+    if (equationList.length == 3) {
+        if (typeof input === "number") {
+            element.value += input;
+        }
+        return;
+    }
+}
+
+function clearDisplay() {
+    const element = document.getElementById("calculatorDisplay");
+    element.value = "";
 }
