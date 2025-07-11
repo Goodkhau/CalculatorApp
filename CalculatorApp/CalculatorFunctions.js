f6e96ca8a53fdfb49b5e389511967729a7d0e528
Goodkhau <gos.khai12@gmail.com>
2025-07-10 20:19:34 -0400

    Finished Project

--- CalculatorApp/CalculatorFunctions.js
+++ CalculatorApp/CalculatorFunctions.js
@@ -1,42 +1,31 @@
 function display(input) {
     const element = document.getElementById("calculatorDisplay");
     if (element.value.length == 0) {
-        if (typeof input === "number") {
+        if (!Number.isNaN(Number(input))) {
             element.value = input;
         }
         return;
     }
 
-    let equationList = element.value.split(" ");
-    if (equationList.length == 1) {
-        if (typeof input === "number") {
-            element.value += input;
-        }
-        if (typeof input === "string") {
-            element.value += " " + input;
-        }
-        return;
-    }
-
-    if (equationList.length == 2) {
-        if (typeof input === "number") {
-            element.value += " " + input;
-        }
-        if (typeof input === "string") {
-            element.value.charAt(element.value.length) = input;
-        }
-        return;
-    }
-
-    if (equationList.length == 3) {
-        if (typeof input === "number") {
-            element.value += input;
-        }
-        return;
-    }
+    element.value += input;
 }
 
 function clearDisplay() {
     const element = document.getElementById("calculatorDisplay");
     element.value = "";
+}
+
+function removeChar() {
+    const element = document.getElementById("calculatorDisplay");
+    element.value = element.value.substring(0,element.value.length-1);
+}
+
+function calculate() {
+    const element = document.getElementById("calculatorDisplay");
+    try {
+        element.value = eval(element.value);
+    }
+    catch (error) {
+        element.value = "Error";
+    }
 }
